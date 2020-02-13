b61c12e7674da525d61959e365c17aab096d0ee3
chrislombaard <chrislombaard@gmail.com>
2020-02-13 18:25:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="App">
+      Hey Kyle. YOU SUCK. 😂
+    </h1>
+  );
+}
+
+export default App;
